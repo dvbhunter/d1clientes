d347b2232969450419accad1ff75fe901eb87ed9
Douglas Vilas Boas <douglas_vilasboas@hotmail.com>
2021-09-22 14:06:17 -0300

    Backend, project created

---
+++ backend/src/main/java/com/devsuperior/d1clientes/D1clientesApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.d1clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D1clientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D1clientesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/d1clientes/D1clientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.d1clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D1clientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
